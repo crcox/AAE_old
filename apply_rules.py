81e5f0433c7aa6bbf19f963a93adfcde96a90b79
Chris <cox.crc@gmail.com>
2014-05-01 12:26:14 -0500

    revising rules (not done)

--- apply_rules.py
+++ apply_rules.py
@@ -1,8 +1,8 @@
 import pickle
 from sys import stdout
 # DEFINITIONS
-vowels = ['a','@','&','E','e','I','i','O','o','U','u','^','w']
-consonants = ['c','d','F','f','G','g','H','h','J','j','K','k',
+vowels = ['c','a','@','&','E','e','I','i','O','o','U','u','^','w']
+consonants = ['d','F','f','G','g','H','h','J','j','K','k',
 			'L','l','M','m','N','n','P','p','Q','q','R','r',
 			's','t','V','v','x','z']
 th = ['T','D']
@@ -55,24 +55,27 @@ with open('change_log','w') as log:
 		# DEVOICING
 		# Rule 1: Final phonemes /b/, /d/, /v/, or /z/ replaced with /p/, /t/,
 		# /f/, and /s/, respectively.
-		if p[-1] in devoicing_rules.keys():
-			rulecode=p[-1]+'-->'+devoicing_rules[p[-1]]
-			log.write(w+": "+''.join(p)+"-->")
-			p[-1] = devoicing_rules[p[-1]]
-			logdict['devoice']['total']+=1
-			try:
-				logdict['devoice'][rulecode]+=1
-			except KeyError:
-				logdict['devoice'][rulecode]=1
+		try:
+			if pp[-2] in vowels and p[-1] in devoicing_rules.keys():
+				rulecode=p[-1]+'-->'+devoicing_rules[p[-1]]
+				log.write(w+": "+''.join(p)+"-->")
+				p[-1] = devoicing_rules[p[-1]]
+				logdict['devoice']['total']+=1
+				try:
+					logdict['devoice'][rulecode]+=1
+				except KeyError:
+					logdict['devoice'][rulecode]=1
 
-			log.write(''.join(p)+"\n")
-			rule_applied[0] = True
+				log.write(''.join(p)+"\n")
+				rule_applied[0] = True
+		except IndexError:
+			pass
 
 		# Consonant Cluster Reduction
 		# Rule 2: If a word ends with a consonant cluster, and the cluster ends
 		# with /t/ /d/ /s/ or /z/, drop it.
 		try:
-			if p[-2] in consonants and p[-1] in ['t','d','s','z']:
+			if pp[-2] in consonants and p[-1] in ['t','d','s','z']:
 				rulecode=p[-1]+'_drop'
 				log.write(w+": "+''.join(p)+"-->")
 				p=p[0:-1]
@@ -89,7 +92,7 @@ with open('change_log','w') as log:
 		# Post-vocalic Reduction
 		# Rule 3: If a word ends with a vowel followed by an /r/, drop the /r/.
 		try:
-			if p[-2] in vowels and p[-1] == 'r':
+			if pp[-2] in vowels and p[-1] == 'r':
 				rulecode=p[-1]+'_drop'
 				log.write(w+": "+''.join(p)+"-->")
 				p=p[0:-1]
